8ba0646318ecc5cd246dc02902b336d060f8c298
MXKXRCH <samakar63@gmail.com>
2022-11-21 12:49:57 +0400

    Added modal window

--- front/src/script.js
+++ front/src/script.js
@@ -11,3 +11,21 @@ function playMusic() {
 	let audio = new Audio('audio.mp3');
 	audio.play();
 }
+
+let modal = document.getElementById('modalWindow');
+let btn = document.getElementById('aboutButton');
+let close = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+close.onclick = function() {
+	modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
